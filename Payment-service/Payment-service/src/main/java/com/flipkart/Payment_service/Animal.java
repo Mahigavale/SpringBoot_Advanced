94d2f97a6b765a23157ed48e1747c8d743044830
Mahigavale <maheshgavale07@gmail.com>
2025-09-18 16:30:12 +0530

    The Springboot and Interview PDF

---
+++ Payment-service/Payment-service/src/main/java/com/flipkart/Payment_service/Animal.java
@@ -0,0 +1,20 @@
+package com.flipkart.Payment_service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Animal  {
+
+	
+	
+	void Aisplay()
+	{
+		System.out.println("Aisplay method...");
+	}
+	
+	void Ais()
+	{
+		System.out.println("Ais method....");
+	}
+
+}
---
+++ Payment-service/Payment-service/src/main/java/com/flipkart/Payment_service/Plants.java
@@ -0,0 +1,19 @@
+package com.flipkart.Payment_service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Plants {
+
+	
+	
+	void display()
+	{
+		System.out.println("Display method...");
+	}
+	
+	void dis()
+	{
+		System.out.println("Dis method....");
+	}
+}
