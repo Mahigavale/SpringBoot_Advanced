de192caaf4fbc68adeec16de7497f3c9e119e668
Mahigavale <maheshgavale07@gmail.com>
2025-10-07 16:23:33 +0530

    DP

--- Delhi_publication - Copy/Delhi_publication/src/main/java/com/penguin_publications/Delhi_publication/Entity/Book.java
+++ Delhi_publication - Copy/Delhi_publication/src/main/java/com/penguin_publications/Delhi_publication/Entity/Book.java
@@ -1,28 +1,26 @@
 package com.penguin_publications.Delhi_publication.Entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 
 @Entity
 public class Book {
 
 	
-	
-	/**
-	 * IDENTITY=> start from 1 and keep increasing by 1
-	 */
-	
-	
+	@Id
+	private int id;
 	public int getId() {
 		return id;
 	}
 
-	
+	public Book(int id, String name, double price, double ratings) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.ratings = ratings;
+
+	}
 
 	public void setId(int id) {
 		this.id = id;
@@ -54,9 +52,8 @@ public class Book {
 
 	
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	
+
 	private String name;
 	private double price;
 	private double ratings;
@@ -64,19 +61,6 @@ public class Book {
 	
 	
 	
-	
-	
-	
-
-	public Book(int id, String name, double price, double ratings) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.ratings = ratings;
-		
-	}
-
 	public Book() {
 		super();
 	}
---
+++ Delhi_publication - Copy/src/test/java/com/penguin_publications/Delhi_publication/DelhiPublicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguin_publications.Delhi_publication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelhiPublicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
